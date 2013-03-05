ae7225684563275d52534a37d1cd575c818ae2b0
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-05 09:31:30 +0700

    Save new user data to a fake DB.

--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -3,30 +3,31 @@
  */
 
 /**
- * Resource generator
+ * Resource generator. Will return a JSON resource if the content-type is not
+ * specified.
  *
- * @param {Object} response express response object
- * @param {Number} statusCode
+ * @param {Object} res express response object
+ * @param {Number} code http status code
  * @param {Object} resource resource in object format
  * @param {String} type resource content type [ json | xml ]
  */
-exports.output = function (response, statusCode, resource, type) {
+exports.output = function (res, code, resource, type) {
 
   var easyxml = require('easyxml');
 
   switch (type) {
     case 'xml':
-      response.set('Content-Type', 'application/xml');
-      response.send(statusCode, easyxml.render(resource));
+      res.set('Content-Type', 'application/xml');
+      res.send(code, easyxml.render(resource));
       break;
 
     case undefined: // unspecified type will return json
     case 'json':
-      response.json(statusCode, resource);
+      res.json(code, resource);
       break;
 
     default:
-      response.json(406, {"message": "Unacceptable request"});
+      res.json(406, {"message": "Unacceptable request"});
   }
 };
 
@@ -41,6 +42,7 @@ exports.output = function (response, statusCode, resource, type) {
  * @api private
  */
 exports.uid = function (len) {
+
   var
     buf = [],
     chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
@@ -63,5 +65,20 @@ exports.uid = function (len) {
  */
 
 function getRandomInt (min, max) {
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * Send email verification link
+ *
+ * @param {String} email unverified email address
+ * @param {Function} cb callback function
+ */
+exports.sendEmailVerification = function (email, cb) {
+
+  if (email === '')
+    cb(new Error('Email is required'), null);
+  else if (email) // TODO send the email using Sendgrid
+    cb(null, true);
+};
