9c3c30d15d835fddf08685e27c2fb4f06e7dd4dd
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-07 02:27:28 +0700

    The sent email content now is contained a fake verification link.

--- src/app/db/authorizationcodes.js
+++ src/app/db/authorizationcodes.js
@@ -1,11 +1,11 @@
 var codes = {};
 
-exports.find = function(key, done) {
+exports.find = function (key, done) {
   var code = codes[key];
   return done(null, code);
 };
 
-exports.save = function(code, clientID, redirectURI, userID, done) {
-  codes[code] = { clientID: clientID, redirectURI: redirectURI, userID: userID };
+exports.save = function (code, clientID, redirectURI, userID, done) {
+  codes[code] = {clientID: clientID, redirectURI: redirectURI, userID: userID};
   return done(null);
 };
