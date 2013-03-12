8c3de39eb5c67cf160a6ed903f3385092a09aab9
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-13 03:47:19 +0700

    Set the API base path to Heroku

--- test/rest-api-end-point-test/oauth2-end-point-spec.js
+++ test/rest-api-end-point-test/oauth2-end-point-spec.js
@@ -7,14 +7,13 @@ var
   OAuth2 = require('oauth').OAuth2,
   vows = require('vows'),
   request = require('request'),
-  assert = require('assert'),
-  app = require('./../../src/app/app');
+  assert = require('assert');
 
-var constructedURI = 'http://localhost:5000/dialog/authorize?' +
+var constructedURI = 'http://sheltered-reef-5266.herokuapp.com/dialog/authorize?' +
   'response_type=code&' +
   'client_id=48c907b0-b8ac-4161-84c9-4fbf1030b5da&' +
   'scope=*&' +
-  'redirect_uri=' + encodeURIComponent('http://localhost:5000/test/callback');
+  'redirect_uri=' + encodeURIComponent('http://sheltered-reef-5266.herokuapp.com/test/callback');
 
 function fillTheLoginForm (uri, cb) {
 
@@ -59,17 +58,17 @@ vows.describe('Scenario: Able to access protected resource')
           topic: function (body) {
             oauth2 = new OAuth2('48c907b0-b8ac-4161-84c9-4fbf1030b5da',
               '48c907b0-dc38-475c-a9c4-4a2e1030b5da', '',
-              'http://localhost:5000/dialog/authorize',
-              'http://localhost:5000/oauth/token');
+              'http://sheltered-reef-5266.herokuapp.com/dialog/authorize',
+              'http://sheltered-reef-5266.herokuapp.com/oauth/token');
 
             oauth2.getOAuthAccessToken(JSON.parse(body).code, {
               grant_type: 'authorization_code',
-              redirect_uri: 'http://localhost:5000/test/callback'
+              redirect_uri: 'http://sheltered-reef-5266.herokuapp.com/test/callback'
             }, this.callback);
           },
           "after get access token": {
             topic: function (accessToken, refreshToken, result) {
-              oauth2.get('http://localhost:5000/say/ghanoz.json',
+              oauth2.get('http://sheltered-reef-5266.herokuapp.com/say/ghanoz.json',
                 accessToken, this.callback);
             },
             "the resource owner should return the actual resource":
