bcacb58d13c5ed336789889dabfb8b9859ea4f4d
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-15 02:46:23 +0700

    Save authorization code to DB

--- src/app/db/users.js
+++ src/app/db/users.js
@@ -1,7 +1,7 @@
 var users = [
   {id: '1', username: 'bob', password: 'secret', name: 'Bob Smith'},
   // special resource owner credentials for test purpose only
-  {id: '2', username: 'joe', password: 'password', name: 'Joe Davis'}
+  {id: '999', username: 'joe', password: 'password', name: 'Joe Davis'}
 ];
 
 exports.find = function (id, cb) {
