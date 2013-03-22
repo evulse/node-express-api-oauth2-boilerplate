7aa615a3f4540c6003ab73482d0e12615bc996a6
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-22 16:23:58 +0700

    EOAB-30 #time 15m Added sample data

--- src/database/express-api-boilerplate-dev.sql
+++ src/database/express-api-boilerplate-dev.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 22, 2013 at 09:35 AM
+-- Generation Time: Mar 22, 2013 at 10:22 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -19,6 +19,58 @@ SET time_zone = "+00:00";
 --
 -- Database: `express-api-boilerplate-dev`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `access_token`
+--
+
+CREATE TABLE IF NOT EXISTS `access_token` (
+  `client_id` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  `token` varchar(256) NOT NULL,
+  KEY `client_id` (`client_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authorization_request`
+--
+
+CREATE TABLE IF NOT EXISTS `authorization_request` (
+  `auth_code` varchar(25) NOT NULL,
+  `redirect_uri` varchar(250) NOT NULL,
+  `client_id` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`auth_code`),
+  KEY `client_id` (`client_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `client_id` varchar(36) NOT NULL,
+  `client_secret` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`client_id`),
+  UNIQUE KEY `client_secret` (`client_secret`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `client_secret`, `user_id`) VALUES
+('c67f0160-7aad-4aa5-8a88-92bbd6f02a4c', '8638be31-2f91-479d-924a-3742feb17443', '912ac711-8b2e-44c6-a088-cb8cf5ab4916');
 
 -- --------------------------------------------------------
 
@@ -50,41 +102,28 @@ INSERT INTO `users` (`id`, `email`, `password`, `confirm_password`, `first_name`
 ('a2bf9b0f-198f-4df5-a396-590a007785bd', 'muhammadghazali2480@gmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Muhammad', 'Ghazali', 'Muhammad Ghazali', 0);
 
 --
--- Table structure for table `client`
+-- Constraints for dumped tables
 --
 
-CREATE TABLE IF NOT EXISTS `clients` (
-  `client_id` varchar(36) NOT NULL,
-  `client_secret` varchar(36) NOT NULL,
-  `user_id` varchar(36) NOT NULL,
-  PRIMARY KEY (`client_id`),
-  UNIQUE KEY `client_secret` (`client_secret`),
-  FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Constraints for table `access_token`
+--
+ALTER TABLE `access_token`
+  ADD CONSTRAINT `access_token_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`),
+  ADD CONSTRAINT `access_token_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
--- --------------------------------------------------------
+--
+-- Constraints for table `authorization_request`
+--
+ALTER TABLE `authorization_request`
+  ADD CONSTRAINT `authorization_request_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`),
+  ADD CONSTRAINT `authorization_request_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-CREATE TABLE IF NOT EXISTS `authorization_request` (
-  `auth_code` varchar(25) NOT NULL,
-  `redirect_uri` varchar(250) NOT NULL,
-  `client_id` varchar(36) NOT NULL,
-  `user_id` varchar(36) NOT NULL,
-  PRIMARY KEY (`auth_code`),
-  FOREIGN KEY (`client_id`) REFERENCES clients(`client_id`),
-  FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `access_token` (
-  `client_id` varchar(36) NOT NULL,
-  `user_id` varchar(36) NOT NULL,
-  `token` varchar(256) NOT NULL,
-  FOREIGN KEY (`client_id`) REFERENCES clients(`client_id`),
-  FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+--
+-- Constraints for table `clients`
+--
+ALTER TABLE `clients`
+  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
