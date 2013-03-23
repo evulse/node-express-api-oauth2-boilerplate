baa67bcb982a0e9ae34f3853f0a9f8146a039b69
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-23 14:29:20 +0700

    EOAB-31 #resolve #time 30m Find access token from DB

--- src/app/models/auth/accesstokens.js
+++ src/app/models/auth/accesstokens.js
@@ -2,6 +2,23 @@ var MySQL = require('./../../db/index').MySQL;
 var db = new MySQL();
 
 /**
+ * Find the saved Access Token
+ *
+ * @param {String} token the token string
+ * @param {Function} cb will return the token records in Array format if found
+ */
+exports.find = function (key, cb) {
+
+  db.connection.query("SELECT * FROM access_token WHERE token = '" + key + "'",
+    function (err, result) {
+      if (err)
+        cb(err);
+      else if (result)
+        cb(null, result);
+    });
+};
+
+/**
  * Save the access token
  *
  * @param {String} token the token string
--- test/unit-test/access-token-spec.js
+++ test/unit-test/access-token-spec.js
@@ -8,6 +8,17 @@ var
 
 var MySQL = require('./../../src/app/db/index').MySQL;
 var db = new MySQL();
+var accessTokenModel = require('./../../src/app/models/auth/accesstokens');
+
+/**
+ * Test suite macros
+ * @param {String} name table name
+ */
+function dropTable (name) {
+  var dropTable = 'DROP TABLE IF EXISTS `' + name + '`;';
+
+  db.connection.query(dropTable, this.callback);
+}
 
 /**
  * Scenario: Save access token
@@ -19,11 +30,7 @@ var db = new MySQL();
 vows.describe('Scenario: Save access token')
   .addBatch({
   'Set Up': {
-    topic: function () {
-      var dropTable = 'DROP TABLE IF EXISTS access_token;';
-
-      db.connection.query(dropTable, this.callback);
-    },
+    topic: dropTable('access_token'),
     'after drop table': {
       topic: function () {
         var createTableStatement = 'CREATE TABLE IF NOT EXISTS `access_token` (' +
@@ -47,9 +54,6 @@ vows.describe('Scenario: Save access token')
   'Given access_token table is empty': {
     'When we save the access token': {
       topic: function () {
-        var accessTokenModel =
-          require('./../../src/app/models/auth/accesstokens');
-
         accessTokenModel.save('0fpDaM01fd6LXl1uXiLHZYIpC3BLNJe9PoVadrB2Pt5HX8LHJUKSPGA1iE0zEcWR6r8Xs3wsSIn5U9wD8DIrZ2iqUYVzyyyg5OvO5B6oFDMJfIUCM2E0H2n4E5sC207ytKzYZ8yElVWNas5boNtu02hSL3kqDeoFDGWzZSUSmMfYlXa29uFeUyAtUY0aDHWgAMIWSnW10X7hxHlFYup9GhjTgAT8iRwXU8EaHYFqGtMG05Zu21OfEKRyF1X3zpw1',
           '912ac711-8b2e-44c6-a088-cb8cf5ab4916',
           'c67f0160-7aad-4aa5-8a88-92bbd6f02a4c', this.callback);
@@ -61,13 +65,34 @@ vows.describe('Scenario: Save access token')
     }
   }
 })
+  .export(module);
+
+/**
+ * Scenario: Find the access_token
+ *
+ * Given acess_token is saved
+ * When find the access_token
+ * Then should return the requested access_token record
+ */
+vows.describe('Scenario: Find the access_token')
+  .addBatch({
+  'Given acess_token is saved': {
+    'When find the access_token': {
+      topic: function () {
+        accessTokenModel.find('0fpDaM01fd6LXl1uXiLHZYIpC3BLNJe9PoVadrB2Pt5HX8LHJUKSPGA1iE0zEcWR6r8Xs3wsSIn5U9wD8DIrZ2iqUYVzyyyg5OvO5B6oFDMJfIUCM2E0H2n4E5sC207ytKzYZ8yElVWNas5boNtu02hSL3kqDeoFDGWzZSUSmMfYlXa29uFeUyAtUY0aDHWgAMIWSnW10X7hxHlFYup9GhjTgAT8iRwXU8EaHYFqGtMG05Zu21OfEKRyF1X3zpw1',
+          this.callback);
+      },
+      'should return the requested access_token record': function (err, result) {
+        assert.isNull(err);
+        assert.isNotNull(result);
+        assert.isArray(result);
+      }
+    }
+  }
+})
   .addBatch({
   'Tear down': {
-    topic: function () {
-      var dropTable = 'DROP TABLE IF EXISTS authorization_codes;';
-
-      db.connection.query(dropTable, this.callback);
-    },
+    topic: dropTable('access_token'),
     'should drop the table': function (err, result) {
       assert.isNull(err);
       assert.isNotNull(result);
@@ -75,11 +100,3 @@ vows.describe('Scenario: Save access token')
   }
 })
   .export(module);
-
-///**
-// * Scenario: Find the access_token
-// *
-// * Given acess_token is saved
-// * When find the access_token
-// * Then should return the requested access_token record
-// */
