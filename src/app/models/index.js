462960b77822dac3dfeffb5171c4ab587f0117a5
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-25 03:13:17 +0700

    Unrequired the dead files

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -2,4 +2,3 @@ exports.users = require('./users');
 exports.clients = require('./clients');
 exports.accessTokens = require('./auth/accesstokens');
 exports.authorizationCodes = require('./auth/authorizationcodes');
-exports.UserModel = require('./usermodel.js').UserModel;
