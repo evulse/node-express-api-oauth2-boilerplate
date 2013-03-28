67adfcfe5beeb793c5c5f7cfcc4031c72ccd724d
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-28 16:11:15 +0700

    EOAB-41 #resolve #time 1h 40m Handle disconnect error

--- src/app/models/clients.js
+++ src/app/models/clients.js
@@ -1,4 +1,5 @@
 var db = require('./../db/index').MySQL;
+var connection = db.getConnection();
 
 /**
  * Find client by client identifier
@@ -8,27 +9,21 @@ var db = require('./../db/index').MySQL;
  */
 exports.findByClientId = function (clientID, cb) {
 
-  db.pool.getConnection(function (err, connection) {
-    if (err && err.code === 'PROTOCOL_CONNECTION_LOST')
-      db.handleDisconnect(connection);
+  connection.query("SELECT * FROM `clients` " +
+    "WHERE client_id = '" + clientID + "'",
+    function (err, result) {
+      if (err) {
+        cb(err);
+      } else if (result[0]) {
+        cb(null, {
+          clientID: result[0].client_id,
+          clientSecret: result[0].client_secret,
+          redirectURI: result[0].redirect_uri,
+          userID: result[0].user_id
+        });
+      }
+    });
 
-    connection.query("SELECT * FROM `clients` " +
-      "WHERE client_id = '" + clientID + "'",
-      function (err, result) {
-        connection.end();
-
-        if (err) {
-          cb(err);
-        } else if (result[0]) {
-          cb(null, {
-            clientID: result[0].client_id,
-            clientSecret: result[0].client_secret,
-            redirectURI: result[0].redirect_uri,
-            userID: result[0].user_id
-          });
-        }
-      });
-  });
 };
 
 exports.save = function (clientID, clientSecret, redirectURI, userID, cb) {
@@ -40,18 +35,11 @@ exports.save = function (clientID, clientSecret, redirectURI, userID, cb) {
     user_id: userID
   };
 
-  db.pool.getConnection(function (err, connection) {
-    if (err && err.code === 'PROTOCOL_CONNECTION_LOST')
-      db.handleDisconnect(connection);
-
-    connection.query('INSERT INTO `clients` SET ?',
-      newClient, function (err, result) {
-      connection.end();
-
-      if (err)
-        cb(err);
-      else if (result.affectedRows && result.affectedRows == 1)
-        cb(null, true);
-    });
+  connection.query('INSERT INTO `clients` SET ?',
+    newClient, function (err, result) {
+    if (err)
+      cb(err);
+    else if (result.affectedRows && result.affectedRows == 1)
+      cb(null, true);
   });
 };
