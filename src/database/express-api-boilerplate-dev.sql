2256224974b7e30bc31c5f964087717e7d5283a4
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-03-29 21:16:20 +0700

    EOAB-39 #resolve #time 20m PDM and implement PDM for verification token related data.

--- src/database/express-api-boilerplate-dev.sql
+++ src/database/express-api-boilerplate-dev.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 24, 2013 at 05:47 PM
+-- Generation Time: Mar 29, 2013 at 03:13 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -50,6 +50,13 @@ CREATE TABLE IF NOT EXISTS `authorization_request` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `authorization_request`
+--
+
+INSERT INTO `authorization_request` (`auth_code`, `redirect_uri`, `client_id`, `user_id`) VALUES
+('sbo2fs3gpHVQWjcE', 'http://localhost:5000/test/callback', 'c67f0160-7aad-4aa5-8a88-92bbd6f02a4c', 'a2bf9b0f-198f-4df5-a396-590a007785bd');
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +72,13 @@ CREATE TABLE IF NOT EXISTS `clients` (
   UNIQUE KEY `client_secret` (`client_secret`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `client_secret`, `redirect_uri`, `user_id`) VALUES
+('c67f0160-7aad-4aa5-8a88-92bbd6f02a4c', '8638be31-2f91-479d-924a-3742feb17443', 'http://localhost:5000/test/callback', '912ac711-8b2e-44c6-a088-cb8cf5ab4916');
 
 -- --------------------------------------------------------
 
@@ -86,6 +100,28 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `confirm_password`, `first_name`, `last_name`, `full_name`, `verified`) VALUES
+('04cbc262-176b-46ea-a91f-09e1793f5d7d', 'duplicate@test.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Duplicate First Name', 'Duplicate Last Name', 'Duplicate Full Name', 1),
+('269f1f69-29a9-4894-b33c-2e1753dd3ddd', 'mike@evulse.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Mike', 'Angell', 'Mike Angell', 1),
+('912ac711-8b2e-44c6-a088-cb8cf5ab4916', 'test@test.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Test first name', 'Test Last name', 'Test full name', 0),
+('a2bf9b0f-198f-4df5-a396-590a007785bd', 'muhammadghazali2480@gmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Muhammad', 'Ghazali', 'Muhammad Ghazali', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verification_token`
+--
+
+CREATE TABLE IF NOT EXISTS `verification_token` (
+  `token` varchar(40) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -101,6 +137,12 @@ ALTER TABLE `access_token`
 --
 ALTER TABLE `clients`
   ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `verification_token`
+--
+ALTER TABLE `verification_token`
+  ADD CONSTRAINT `verification_token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
